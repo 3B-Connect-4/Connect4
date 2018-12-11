e2ef26f0356012f494945a12e36c3eb068d7efb6
Biddulph, Caleb D <130435@s020rmc148-02.local>
2018-12-11 14:28:59 -0600

    Trying to commit those comments again

--- Connect4/Game.swift
+++ Connect4/Game.swift
@@ -17,4 +17,42 @@ class Game {
     init() {
         tokenGrid = Array(repeating: Array(repeating: Player.PlayerID.NONE, count: 6), count: 7)
     }
+    
+    //precondition: the row and col should correspond to an already-existing token on the top of a column
+    /*func causedAWin(col: Int, row: Int) -> Bool {
+        let p = tokenGrid[col][row] //the player to evaluate for a win
+        if p == Player.PlayerID.NONE {
+            return false; //returns false if there is not already a token there
+        }
+        
+        //keeps track of number of matching tokens in each direction
+        //0 moves up (should always be zero...), 1 is up-right, 2 is right, etc. to 7 as up-left
+        var elimDirs = Array(repeating: 0, count: 8)
+        for i in 0...3 {
+            if elimDirs[0] == i && tokenGrid[col][row + 1] == p {
+                elimDirs[0] = i + 1
+            }
+            if elimDirs[1] == i && tokenGrid[col + 1][row + 1] == p {
+                elimDirs[1] = i + 1
+            }
+            if elimDirs[2] == i && tokenGrid[col + 1][row] == p {
+                elimDirs[2] = i + 1
+            }
+            if elimDirs[3] == i && tokenGrid[col + 1][row - 1] == p {
+                elimDirs[3] = i + 1
+            }
+            if elimDirs[4] == i && tokenGrid[col][row - 1] == p {
+                elimDirs[4] = i + 1
+            }
+            if elimDirs[5] == i && tokenGrid[col - 1][row - 1] == p {
+                elimDirs[5] = i + 1
+            }
+            if elimDirs[6] == i && tokenGrid[col - 1][row] == p {
+                elimDirs[6] = i + 1
+            }
+            if elimDirs[7] == i && tokenGrid[col - 1[row + 1] == p {
+                elimDirs[7] = i + 1
+            }
+        }*/
+    }
 }
