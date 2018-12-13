4d3fb6ad11c3103c874bb5d0fccf38128a169d53
Biddulph, Caleb D <130435@s020rmc148-02.local>
2018-12-13 13:32:17 -0600

    Added causedAWin method to determine if the last-placed piece caused four-in-a-row

--- Connect4/Game.swift
+++ Connect4/Game.swift
@@ -14,8 +14,11 @@ class Game {
     //tokenGrid[0][0] is the bottommost token in the leftmost column, and tokenGrid[0][5] is the top left token
     var tokenGrid: [[Player.PlayerID]]
     
+    let numRows = 6
+    let numCols = 7
+    
     init() {
-        tokenGrid = Array(repeating: Array(repeating: Player.PlayerID.NONE, count: 6), count: 7)
+        tokenGrid = Array(repeating: Array(repeating: Player.PlayerID.NONE, count: numRows), count: numCols)
     }
     func reset(){
         for row in 0...5 {
@@ -27,7 +30,7 @@ class Game {
     }
     
     //precondition: the row and col should correspond to an already-existing token on the top of a column
-    /*func causedAWin(col: Int, row: Int) -> Bool {
+    func causedAWin(col: Int, row: Int) -> Bool {
         let p = tokenGrid[col][row] //the player to evaluate for a win
         if p == Player.PlayerID.NONE {
             return false; //returns false if there is not already a token there
@@ -36,31 +39,46 @@ class Game {
         //keeps track of number of matching tokens in each direction
         //0 moves up (should always be zero...), 1 is up-right, 2 is right, etc. to 7 as up-left
         var elimDirs = Array(repeating: 0, count: 8)
+        for i in 1...3 {
+            if elimDirs[0] == i - 1 && isMatchingToken(col: col, row: row + 1, matches: p) {
+                elimDirs[0] = i
+            }
+            if elimDirs[1] == i - 1 && isMatchingToken(col: col + 1, row: row + 1, matches: p) {
+                elimDirs[1] = i
+            }
+            if elimDirs[2] == i - 1 && isMatchingToken(col: col + 1, row: row, matches: p) {
+                elimDirs[2] = i
+            }
+            if elimDirs[3] == i - 1 && isMatchingToken(col: col + 1, row: row - 1, matches: p) {
+                elimDirs[3] = i
+            }
+            if elimDirs[4] == i - 1 && isMatchingToken(col: col, row: row - 1, matches: p) {
+                elimDirs[4] = i
+            }
+            if elimDirs[5] == i - 1 && isMatchingToken(col: col - 1, row: row - 1, matches: p) {
+                elimDirs[5] = i
+            }
+            if elimDirs[6] == i - 1 && isMatchingToken(col: col - 1, row: row, matches: p) {
+                elimDirs[6] = i
+            }
+            if elimDirs[7] == i - 1 && isMatchingToken(col: col - 1, row: row + 1, matches: p) {
+                elimDirs[7] = i
+            }
+        }
+        
+        //adds up opposite directions, returns true if any sum to 3 or more (the middle token is not counted to determine four-in-a-row)
         for i in 0...3 {
-            if elimDirs[0] == i && tokenGrid[col][row + 1] == p {
-                elimDirs[0] = i + 1
+            if elimDirs[i] + elimDirs[i+4] >= 3 {
+                return true
             }
-            if elimDirs[1] == i && tokenGrid[col + 1][row + 1] == p {
-                elimDirs[1] = i + 1
-            }
-            if elimDirs[2] == i && tokenGrid[col + 1][row] == p {
-                elimDirs[2] = i + 1
-            }
-            if elimDirs[3] == i && tokenGrid[col + 1][row - 1] == p {
-                elimDirs[3] = i + 1
-            }
-            if elimDirs[4] == i && tokenGrid[col][row - 1] == p {
-                elimDirs[4] = i + 1
-            }
-            if elimDirs[5] == i && tokenGrid[col - 1][row - 1] == p {
-                elimDirs[5] = i + 1
-            }
-            if elimDirs[6] == i && tokenGrid[col - 1][row] == p {
-                elimDirs[6] = i + 1
-            }
-            if elimDirs[7] == i && tokenGrid[col - 1[row + 1] == p {
-                elimDirs[7] = i + 1
-            }
-        }*/
+        }
+        return false
+    }
+    
+    func isMatchingToken(col: Int, row: Int, matches player: Player.PlayerID) -> Bool {
+        if col < 0 || col >= numCols || row < 0 || row >= numRows {
+            return false
+        }
+        return tokenGrid[col][row] == player
     }
 }
